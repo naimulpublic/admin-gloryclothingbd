f27ac60061c920fd7d3e85e38275c5e087c38fdc
naimul islam <naimul.stu@gmail.com>
2025-08-07 23:03:13 +0600

    add new iame fild

--- src/components/createproduct/ProductCreatePage.jsx
+++ src/components/createproduct/ProductCreatePage.jsx
@@ -45,6 +45,7 @@ export default function ProductForm({
   const [boxContent, setBoxContent] = useState([{ key: "", value: "" }]);
   const [tags, setTags] = useState([]);
   const [selectsubchild, setSelectsubchild] = useState([]);
+  const [measurementImage, setMeasurementImage] = useState(null);
 
   const [colorVariants, setColorVariants] = useState([
     {
@@ -57,6 +58,17 @@ export default function ProductForm({
   const optionssubchild = subChild || [];
 
   const [isLoading, setIsLoading] = useState(false);
+  const [previewUrl, setPreviewUrl] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setMeasurementImage(file);
+
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setPreviewUrl(url);
+    }
+  };
 
   useEffect(() => {
     if (id) {
@@ -82,6 +94,11 @@ export default function ProductForm({
             setCategorySlug(data.categorySlug || "");
             setDefaultColor(data.defaultColor || "");
             setIsFeatured(data.isFeatured || false);
+            setMeasurementImage(data.measurementImage);
+
+            if (data.measurementImage) {
+              setPreviewUrl(data.measurementImage);
+            }
 
             setSubcategory(
               Array.isArray(data.subcategory)
@@ -182,6 +199,7 @@ export default function ProductForm({
     formData.append("categorySlug", categorySlug);
     formData.append("defaultColor", defaultColor);
     formData.append("isFeatured", isFeatured);
+    formData.append("measurementImage", measurementImage);
 
     formData.append("subcategory", JSON.stringify(subcategory));
     formData.append("specification", JSON.stringify(specification));
@@ -805,23 +823,37 @@ export default function ProductForm({
           Tags for best seo Performence
         </h4>
 
-        <div className="relative">
-          <input
-            id="floating_tag"
-            value={tags.join(",")}
-            onChange={(e) =>
-              setTags(e.target.value.split(",").map((t) => t.trim()))
-            }
-            type="text"
-            className="block px-2.5 pb-2 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-          />
-          <label
-            htmlFor="floating_tag"
-            className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-          >
-            Enter tags separated by commas{" "}
-          </label>
+        <div className="flex items-center ">
+          <div className="relative w-1/2">
+            <input
+              id="floating_tag"
+              value={tags.join(",")}
+              onChange={(e) =>
+                setTags(e.target.value.split(",").map((t) => t.trim()))
+              }
+              type="text"
+              className="block px-2.5 pb-2 pt-3 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="floating_tag"
+              className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
+            >
+              Enter tags separated by commas{" "}
+            </label>
+          </div>
+          <div className="w-1/2 p-2">
+            <input onChange={handleFileChange} type="file" />
+            {previewUrl && (
+              <img
+                src={previewUrl}
+                alt="Measurement Preview"
+                width={200}
+                height={200}
+                className="w-14 h-10"
+              />
+            )}
+          </div>
         </div>
       </div>
 
