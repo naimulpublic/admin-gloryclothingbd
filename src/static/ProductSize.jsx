4d73e2b14becfa9726673826975cabb41a84e36f
naimul islam <naimul.stu@gmail.com>
2025-08-08 22:24:24 +0600

    add size for required

--- src/static/ProductSize.jsx
+++ src/static/ProductSize.jsx
@@ -1,16 +1,32 @@
-export 
-const productSizes = [
-  { title: "clothing", value: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"] },
-  { title: "shoes", value: ["5", "6", "7", "8", "9", "10", "11"] },
-  { title: "jeans", value: ["26", "28", "30", "32", "34", "36", "38", "40"] },
-  { title: "bedding", value: ["Single", "Twin", "Full", "Queen", "King"] },
+export const productSizes = [
   {
-    title: "phone_cases",
-    value: ["4.7 inch", "5.5 inch", "6.1 inch", "6.5 inch"],
+    title: "Men's Women's Tops",
+    value: ["XS", "S", "M", "L", "XL", "XXL", "3XL", "5xl", "6xl"],
   },
-  { title: "accessories", value: ["Small", "Medium", "Large"] },
-  { title: "electronics", value: ["Small", "Medium", "Large"] },
-  { title: "hats", value: ["S", "M", "L", "XL"] },
-  { title: "bags", value: ["Small", "Medium", "Large"] },
-  { title: "gloves", value: ["XS", "S", "M", "L", "XL"] },
+  {
+    title: "Men's Women's Bottom",
+    value: [
+      "28",
+      "30",
+      "32",
+      "34",
+      "36",
+      "38",
+      "40",
+      "42",
+      "44",
+      "46",
+      "48",
+      "50",
+    ],
+  },
+  {
+    title: "Kid's Baby's",
+    value: ["3m", "6m", "9m", "12m", "15m", "18m", "24", "36"],
+  },
+  { title: "Boy's & Girl's", value: ["2y", "4y", "6y", "8y", "10y"] },
+  {
+    title: "Adult Boy's & Girl's",
+    value: ["6y", "8y", "10y", "12y", "14y", "16y", "18y"],
+  },
 ];
